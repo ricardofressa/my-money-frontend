7668f12c2b3848484a59acc929934ac57d709575
Ricardo Cardoso <ricardo@Ricardos-MacBook-Pro.local>
2018-12-04 17:35:10 -0200

    Implemented Authentication login

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
+import AuthOrApp from './authOrApp'
 
 import App from './app'
 import Dashboard from '../dashboard/dashboard'
@@ -7,7 +8,7 @@ import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={App}>
+        <Route path='/' component={AuthOrApp}>
             <IndexRoute component={Dashboard} />
             <Route path='billingCycles' component={BillingCycle} />
         </Route>
